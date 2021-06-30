11c67b135dfd33e95c9328b5d6393102ad834055
unknown <chtsweiliang@DESKTOP-4R57A75.CDSAFI.COM>
2021-06-30 21:51:30 +0800

    hello method , Person class

--- src/com/cht/Hello.java
+++ src/com/cht/Hello.java
@@ -5,37 +5,33 @@ import java.util.Random;
 public class Hello {
 
 	public static void main(String[] args) {
+
+//		System.out.println("Hello World!");
+		Person william = new Person("william");
+		william.hello();
 		// 快速鍵: alt+/
 		// 刪除: ctrl+D
 		// 執行Java: ctrl+F11
 		// 字體大小: Ctrl+"+"或"-"
 		// 改變數名稱:Refactor->rename
+		// F3:快速進入變數定義的地方
 //		System.out.println("Hello world!");
-		Random random = new Random();
-		int number = random.nextInt();
-		long big = 11234424445555L;
-		double weight = 85.4d;
-		float height = 173.5f;
-		byte b = 12;
-		char c = 'A';
-		char c2 = 123;
-		boolean adult = false;
-		boolean paid = true;
-		System.out.println(number);
-		System.out.println(weight);
-		System.out.println(height);
-
-		// 類別與物件的關係
-		String data = "123";
-		System.out.println(Integer.parseInt(data) + 1);
-
-		int num = 6;
-		num = num + 2; // 8
-		num = num - 5; // 3
-		num = num /2 ; // ?
-
-		System.out.println(num);
-
+		/*
+		 * Random random = new Random(); int number = random.nextInt(); long big =
+		 * 11234424445555L; double weight = 85.4d; float height = 173.5f; byte b = 12;
+		 * char c = 'A'; char c2 = 123; boolean adult = false; boolean paid = true;
+		 * System.out.println(number); System.out.println(weight);
+		 * System.out.println(height);
+		 * 
+		 * // 類別與物件的關係 String data = "123"; System.out.println(Integer.parseInt(data) +
+		 * 1);
+		 * 
+		 * int num = 6; num = num + 2; // 8 num = num - 5; // 3 num = num /2 ; // ?
+		 * 
+		 * System.out.println(num);
+		 * 
+		 * int a = 6 ; System.out.println(a%4);
+		 */
 	}
 
 }
--- src/com/cht/Person.java
+++ src/com/cht/Person.java
@@ -2,4 +2,14 @@ package com.cht;
 
 public class Person {
 
+	String name;
+
+	public Person(String name) {
+		this.name = name;
+	}
+
+	public void hello() {
+		System.out.println(name + ", Hello!");
+	}
+
 }
